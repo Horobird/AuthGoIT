4d512ddcd5728471db3b5c2975c9c1c239854835
Konstantyn_Gorobets <horobird@gmail.com>
2023-06-20 17:21:49 +0300

    SelectorCorrection
    Selector of menu correction

--- cypress/e2e/Auth/pages/Login.js
+++ cypress/e2e/Auth/pages/Login.js
@@ -1,6 +1,6 @@
 export class Login {
   selectorMenu = "#open-navigation-menu-mobile";
-  selectorLogOut = ":nth-child(9) > .css-bve2vl";
+  selectorLogOut = ":nth-child(11) > .css-bve2vl";
   navPage() {
     cy.get(this.selectorMenu).click();
     cy.get(this.selectorLogOut).click();
